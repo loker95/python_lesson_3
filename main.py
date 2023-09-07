f8d1a1f78a3e464593723258ec31e12b560698d1
nadiiahubatenko <loker1995@gmail.com>
2023-09-07 11:21:27 +0100

    added and completed task 1

--- main.py
+++ main.py
@@ -2,3 +2,30 @@
 # dsjgjdjfgdjgjdfg
 # develop commit
 # some information
+
+# develop commit
+
+n1 = int(input("введите первую цифру "))
+n2 = int(input("введите вторую цифру "))
+n3 = int(input("введите третью цифру "))
+
+user_choice = int(input("введите 1, чтобы узнать максимальную цифру\n2, чтобы узнать минимальную цифру\n3, чтобы узнать среднее арифметическое чисел\n"))
+
+if user_choice == 1:
+    if n1 >= n2 >= n3:
+        print(n1)
+    elif n2 >= n1 >= n3:
+        print(n2)
+    else:
+        print(n3)
+elif user_choice == 2:
+    if n1 <= n2 <= n3:
+        print(n1)
+    elif n2 <= n1 <= n3:
+        print(n2)
+    else:
+        print(n3)
+elif user_choice == 3:
+    print((n1 + n2 + n3) / 3)
+else:
+    print("Неверный выбор")
