9695d0a20e0bf125cb7dc80c6b2add6789bd81aa
nadiiahubatenko <loker1995@gmail.com>
2023-09-07 11:53:11 +0100

    сompleted second task

--- main.py
+++ main.py
@@ -26,9 +26,21 @@ elif user_choice == 2:
     else:
         print(n3)
 elif user_choice == 3:
-    print((n1 + n2 + n3) / 3)
+    print(int((n1 + n2 + n3) / 3))
 else:
     print("Неверный выбор")
     
 # ############################
 # ############################
+
+length = int(input("Введите длину в метрах "))
+
+user_convert_choice = int(input("1 - перевести в мили,\n2 -в дюймы,\n3 в ярды\n"))
+
+if user_convert_choice == 1:
+    print(length * 0.000621371, "миль")
+elif user_convert_choice == 2:
+    print(length * 39.370, "дюймов")
+elif user_convert_choice == 3:
+    print(length * 1.0936, "ярдов")
+else: print("Введите число из предложенных")
